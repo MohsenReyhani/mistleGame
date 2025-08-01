64ea6de7adb7cfe4be3ae25ca3c0efc2a1404e9f
MohsenReyhani <mohsenreyhani@gmail.com>
2025-08-01 21:24:52 +0330

    fixed size

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
 this is a simple game with phazer!
+
+run locally: 
+ npx http-server . -p 8000 -c-1
+ 
--- main.js
+++ main.js
@@ -1,11 +1,28 @@
 // main.js
 const config = {
+
   type: Phaser.AUTO,
-  width: 480,
-  height: 640,
-  scale: { mode: Phaser.Scale.FIT, autoCenter: Phaser.Scale.CENTER_BOTH },
+  parent: 'game-container',
+  scale: {
+    mode: Phaser.Scale.FIT,      // or Phaser.Scale.RESIZE
+    autoCenter: Phaser.Scale.CENTER_BOTH,
+    width: window.innerWidth,    // initial size
+    height: window.innerHeight,
+  },
   physics: { default: 'arcade', arcade: { gravity: { y: 600 } } },
-  audio: { disableWebAudio: true },    // ← use HTML5 Audio
   scene: [ BootScene, LoadingScene, MenuScene, GameScene, GameOverScene ],
+
+
+  // type: Phaser.AUTO,
+  // width: 480,
+  // height: 640,
+  // scale: { mode: Phaser.Scale.FIT, autoCenter: Phaser.Scale.CENTER_BOTH },
+  // physics: { default: 'arcade', arcade: { gravity: { y: 600 } } },
+  // audio: { disableWebAudio: true },    // ← use HTML5 Audio
 };
-new Phaser.Game(config);
+
+const game = new Phaser.Game(config);
+window.addEventListener('resize', () => {
+  game.scale.resize(window.innerWidth, window.innerHeight);
+});
+
